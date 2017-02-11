37c1508f1684e92e2b4b510f62189370817238cb
delzak <lex_909@mail.ru>
2017-02-11 23:08:28 +0300

    Добавился вывод координат каждой из созданных точек

--- sandbox/src/main/java/ru/stqa/pft/homework/Main.java
+++ sandbox/src/main/java/ru/stqa/pft/homework/Main.java
@@ -7,7 +7,9 @@ public class Main {
         Point p1 = new Point(20, 20);
         Point p2 = new Point(40, 40);
         CalculateDistance d = new CalculateDistance();
-        System.out.println("Расстояние между двумя точками = " + d.distance(p1, p2));
+        System.out.println("Точка 1 имеет координаты x = " + p1.x + " и y = " + p1.y + "\n" +
+                           "Точка 2 имеет координаты x = " + p2.x + " и y = " + p2.y + "\n" +
+                           "Расстояние между двумя точками = " + d.distance(p1, p2));
     }
 
 }
