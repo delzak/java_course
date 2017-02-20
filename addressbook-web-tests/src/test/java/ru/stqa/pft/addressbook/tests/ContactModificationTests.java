9d610acd66ab46541911702001fdf6610f59884c
delzak <lex_909@mail.ru>
2017-02-20 17:42:39 +0300

    В рамках домашнего задания был создан тест-класс ContactModificationTests для изменения информации о контакте. Был доработан ContactHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactInformation;
+
+/**
+ * Created by Алексей on 20.02.2017.
+ */
+public class ContactModificationTests extends TestBase{
+
+    @Test
+    public void testContactModification(){
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().inputContactInformation(new ContactInformation("Imya", "Familiya", "telefon", "kakoy-to@email.com", "Gde contact givet"));
+        app.getContactHelper().submitContactModification();
+        app.getContactHelper().returnToContactPage();
+    }
+}
