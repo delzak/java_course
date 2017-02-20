ba1d3ec09177e34b654d9cbb34cb8ac2f159d0b8
delzak <lex_909@mail.ru>
2017-02-20 15:45:07 +0300

    Небольшой рефакторинг кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,85 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactInformation;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-
-public class ContactCreationTests {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+public class ContactCreationTests extends TestBase{
 
     @Test
     public void testContactCreation() {
-        clickAddNewContact();
-        inputContactInformation(new ContactInformation("Alex", "Javov", "89876543210", "anemail@mail.com", "Any Town, Any Street, Any number house"));
-        submitContactInformation();
-        returnToContactPage();
+        app.gotoHomePage();
+        app.clickAddNewContact();
+        app.inputContactInformation(new ContactInformation("Alex", "Javov", "89876543210", "anemail@mail.com", "Any Town, Any Street, Any number house"));
+        app.submitContactInformation();
+        app.returnToContactPage();
     }
 
-    private void returnToContactPage() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-
-    private void submitContactInformation() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    private void inputContactInformation(ContactInformation contactInformation) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactInformation.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactInformation.getLastname());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(contactInformation.getTelephone());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactInformation.getEmail());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactInformation.getAddress());
-
-    }
-
-    private void clickAddNewContact() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -6,6 +6,9 @@ public class ContactDeletionTests extends TestBase{
 
     @Test
     public void testContactDeletion() {
+        app.gotoHomePage();
+        app.selectContact();
+        app.deleteSelectedContacts();
+        app.gotoHomePage();
     }
-
 }
