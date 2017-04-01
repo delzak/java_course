cfb797e5ee8be19a57f36dbaf8e95b7e4a2bef86
delzak <lex_909@mail.ru>
2017-04-01 18:07:53 +0300

    Доработан тест регистрации
    добавлены классы HelperBase. MailHelper, MailMessage

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -34,7 +34,7 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpclient.execute(post);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
+        return body.contains(String.format("<a href=\"/mantisbt-2.2.1/account_page.php\">%s</a>", username));
     }
 
     private String geTextFrom(CloseableHttpResponse response) throws IOException{
@@ -49,6 +49,6 @@ public class HttpSession {
         HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
         CloseableHttpResponse response = httpclient.execute(get);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"label hidden-xs label-default arrowed\">%s</span>", username));
+        return body.contains(String.format("<a href=\"/mantisbt-2.2.1/account_page.php\">%s</a>", username));
     }
 }
